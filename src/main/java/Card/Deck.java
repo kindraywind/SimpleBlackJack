fc2b6dcf33c5bb806cec4724d480ff1eed18e83c
Woramet Muangsiri <krw127@hotmail.com>
2015-12-20 21:54:50 +0900

    Add DeckTest and impl

--- src/main/java/Card/Deck.java
+++ src/main/java/Card/Deck.java
@@ -1,7 +1,43 @@
 package Card;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by woramet on 12/20/15.
  */
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        resetAndShuffle();
+    }
+
+    private void resetAndShuffle() {
+        cards = new ArrayList<Card>();
+        Card c;
+
+        for (Suit s: Suit.values()) {
+            for (CardValue v: CardValue.values()) {
+                c = new Card(s, v);
+                cards.add(c);
+            }
+        }
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCardFromTheTop() {
+        if (isEmpty()) return null;
+
+        return cards.remove(0);
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+
+    public int getCardCount() {
+        return cards.size();
+    }
 }
