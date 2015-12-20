6cd28932bc06e05bab0644f28ff9953cfe9b709d
Woramet Muangsiri <krw127@hotmail.com>
2015-12-20 20:40:00 +0900

    Initialize project Hello worlddddd

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by woramet on 12/20/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello world");
+    }
+}
+
