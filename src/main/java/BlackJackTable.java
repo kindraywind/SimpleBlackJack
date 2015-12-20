2dcfc57656c07e99065ae91a467d6382e4d03f4d
Woramet Muangsiri <krw127@hotmail.com>
2015-12-20 20:59:25 +0900

    Add constructor for BlackJackTable

--- src/main/java/BlackJackTable.java
+++ src/main/java/BlackJackTable.java
@@ -2,4 +2,17 @@
  * Created by woramet on 12/20/15.
  */
 public class BlackJackTable {
+
+    private Dealer dealer;
+    private Gambler gambler;
+    private Deck cardDeck;
+
+    public BlackJackTable(Dealer dealer, Gambler gambler) {
+        this.dealer = dealer;
+        this.gambler = gambler;
+    }
+
+    public void startTheGame() {
+        
+    }
 }
